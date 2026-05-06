f1b2c7c0e72f262d29ae8c97ac505c42ed5c5034
poomichopoom-p <Poomicho.p@gmail.com>
2026-05-06 12:41:41 +0700

    addHeroButton

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "./assets/vite.svg";
+// import heroImg from "./assets/hero.png";
 import "./App.css";
 import ButtonMain from "./componente/ButtonMain";
+import HeroButton from "./componente/HeroButton";
 
 function App() {
 
   return (
     <>
       <ButtonMain />
+      <HeroButton />
     </>
   );
 }
---
+++ src/componente/HeroButton.jsx
@@ -0,0 +1,13 @@
+
+
+export default function HeroButton(){
+    
+
+    return(
+        <>
+        <button className="bg-[#C3FF51] text-[#080809] w-[100px] h-[40px] rounded-[20px] hover:bg-[#080809] border-[]">
+            button
+        </button>
+        </>
+    )
+}
