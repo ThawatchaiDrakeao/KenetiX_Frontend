29fa0244ff1a51ee87b9ccadb60c849b4231f21c
poomichopoom-p <Poomicho.p@gmail.com>
2026-06-11 00:56:36 +0700

    FixPath

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { LanguageProvider } from "./context/LanguageProvider";
-import { AuthProvider } from "./context/AuthContext";
-import { CartProvider } from './context/CartContext.jsx';
-import './index.css';
-import App from './App.jsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+import { LanguageProvider } from "./context/LanguageProvider.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
+import { CartProvider } from "./context/CartContext.jsx";
+import "./index.css";
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <LanguageProvider>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,26 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import Navbar from '../components/Navbar';
-import Hero from '../components/Hero';
-import Features from '../components/Features';
-import HowItWorksSection from '../components/HowItWorksSection';
-import HowItWorksPage from '../pages/HowItWorksPage';
-import Showcase from '../components/Showcase';
-import Reviews from '../components/Reviews';
-import Pricing from '../components/Pricing';
-import Community from '../components/Community';
-import OurStory from '../components/OurStory';
-import FAQ from '../components/FAQ';
-import ContactPage from '../pages/ContactPage';
-import Catalog from '../pages/Catalog';
-import CTA from '../components/CTA';
-import Footer from '../components/Footer';
-import Login from "../pages/Login";
-import SignupPage from "../components/SignupPage";
-import ScrollArrow from "../components/ScrollArrow";
-import UserDashboard from "../pages/UserDashboard"
+import Navbar from '../components/Navbar.jsx';
+import Hero from '../components/Hero.jsx';
+import Features from '../components/Features.jsx';
+import HowItWorksSection from '../components/HowItWorksSection.jsx';
+import HowItWorksPage from '../pages/HowItWorksPage.jsx';
+import Showcase from '../components/Showcase.jsx';
+import Reviews from '../components/Reviews.jsx';
+import Pricing from '../components/Pricing.jsx';
+import Community from '../components/Community.jsx';
+import OurStory from '../components/OurStory.jsx';
+import FAQ from '../components/FAQ.jsx';
+import ContactPage from '../pages/ContactPage.jsx';
+import Catalog from '../pages/Catalog.jsx';
+import CTA from '../components/CTA.jsx';
+import Footer from '../components/Footer.jsx';
+import Login from "../pages/Login.jsx";
+import SignupPage from "../components/SignupPage.jsx";
+import ScrollArrow from "../components/ScrollArrow.jsx";
+import UserDashboard from "../pages/UserDashboard.jsx"
 //import CatalogNavbar from "../components/catalog/CatalogNavbar";
-import CheckOut from "./CheckOut";
-import OrderConfirmation from "./OrderConfirmation";
+import CheckOut from "./CheckOut.jsx";
+import OrderConfirmation from "./Orderconfirmation.jsx";
 
 function HomeContent() {
   return (
