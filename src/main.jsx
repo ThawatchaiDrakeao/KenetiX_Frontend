b4567031f489980bfe802d52585d74b0c575afb5
alisabunnak <alisabunnak.2539@gmail.com>
2026-06-11 14:01:14 +0700

    Edit main

--- src/main.jsx
+++ src/main.jsx
@@ -10,12 +10,6 @@ import { NotificationProvider } from "./context/NotificationContext.jsx";
 import "./index.css";
 import App from "./App.jsx";
 
-import { LanguageProvider } from "./context/LanguageProvider.jsx";
-import { AuthProvider } from "./context/AuthContext.jsx";
-import { CartProvider } from "./context/CartContext.jsx";
-import "./index.css";
-import App from "./App.jsx";
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
